782ad30dfd2ce7dc04beaf309b1290fe63ac6d44
Nimpoo <marwan.ayoub.pro@gmail.com>
2025-10-03 19:27:16 +0200

    Part 3 - `p3`: `clsuster-launcher.sh` script for automates the cluster creation

--- p3/scripts/cluster-launcher.sh
+++ p3/scripts/cluster-launcher.sh
@@ -35,7 +35,30 @@ sudo k3d cluster create --no-lb --agents 0 p3-cluster
 sudo kubectl create namespace dev
 sudo kubectl create namespace argocd
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 # ! PORT FORWARDING, BLOQUE LE PROMPT, LANCE LE DASHBOARD ARGO CD SUR LE PORT 8080
+# ? I - Lancer cette commande pour port forwarding le dashboard Argo CD sur le port 8080 de la machine hôte et y accéder via http://localhost:8080
+# * ```
+# * sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+# * ```
+# ! PORT FORWARDING, BLOQUE LE PROMPT, LANCE LE DASHBOARD ARGO CD SUR LE PORT 8080
 
-# // TODO: SETUP LE CLUSTER AVEC K3D, CREATION DU NAMESPACE "argocd", INSTALLATION DE ARGO CD DANS LE CLUSTER ET CONTINUER A VOIR CE QU'IL RESTE A FAIRE
-# TODO: A PARTIR DE CETTE DERNIERE LIGNE DE COMMANDE, CONTINUER LA DOC D'ARGOCD A PARTIR DE LA: https://argo-cd.readthedocs.io/en/stable/getting_started/#4-login-using-the-cli
+# ? II - Récupérer le mot de passe initial pour l'utilisateur admin
+# * ```
+# * sudo argocd admin initial-password -n argocd
+# * ```
+# ? Se connecter au dashboard + CLI avec l'utilisateur admin et le mot de passe récupéré
+
+# ? III - Supprimer le `secret` qui stock le mot de passe initial pour des raisons de sécurité
+# * ```
+# * sudo kubectl delete secret -n argocd argocd-initial-admin-secret
+# * ```
+
+# ? IV - Changer le mot de passe de l'utilisateur admin
+# * ```
+# * sudo argocd account update-password
+# * ```
+
+# ? V - Ajouter le cluster k3d au CLI Argo CD
+# * ```
+# * sudo argocd cluster add k3d-p3-cluster
+# * ```
+# ! S'il y a un probleme lors de l'ajout du cluster k3d au CLI Argo CD, consulter : https://github.com/Nimpoo/Inception-of-Things/wiki/V-%E2%80%90-ArgoCD,-CI-CD,-GitOps-Model#warning
