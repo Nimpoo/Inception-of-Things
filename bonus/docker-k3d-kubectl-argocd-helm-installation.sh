3094301a0f084df624a45ab4249c6f2a6b0e8271
Nimpoo <marwan.ayoub.pro@gmail.com>
2025-12-02 13:54:16 +0100

    feat: add installation and uninstallation scripts for Docker, kubectl, K3d, argocd, and helm

---
+++ bonus/docker-k3d-kubectl-argocd-helm-installation.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
+# Docker installation
+sudo dnf remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-selinux \
+                docker-engine-selinux \
+                docker-engine
+
+curl -fsSL https://get.docker.com -o install-docker.sh
+
+sudo sh install-docker.sh
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo rm -rfv install-docker.sh
+
+# kubectl installation
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+if echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check; then
+  echo "kubectl checksum is valid"
+else
+  echo "kubectl checksum is invalid"
+  exit 1
+fi
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo rm -rfv kubectl kubectl.sha256
+
+# K3d installation
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+# argocd installation
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
+
+# helm installation
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
---
+++ bonus/docker-k3d-kubectl-argocd-helm-uninstallation.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+
+command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+
+get_distribution() {
+    lsb_dist=""
+    if [ -r /etc/os-release ]; then
+        lsb_dist="$(. /etc/os-release && echo "$ID")"
+    fi
+    echo "$lsb_dist"
+}
+
+do_uninstall() {
+    echo "# Démarrage du script de désinstallation de Docker"
+    user="$(id -un 2>/dev/null || true)"
+    sh_c='sh -c'
+    if [ "$user" != 'root' ]; then
+        if command_exists sudo; then
+            sh_c='sudo -E sh -c'
+        elif command_exists su; then
+            sh_c='su -c'
+        else
+            cat >&2 <<-'EOF'
+Erreur : Ce script nécessite les privilèges root pour fonctionner.
+Nous ne trouvons ni "sudo" ni "su" pour obtenir ces privilèges.
+EOF
+            exit 1
+        fi
+    fi
+
+    lsb_dist=$(get_distribution)
+    lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+
+    case "$lsb_dist" in
+        ubuntu|debian|raspbian)
+            (
+                echo "Désinstallation des packages Docker..."
+                $sh_c 'apt-get remove -y docker docker-engine docker.io containerd runc'
+                $sh_c 'apt-get purge -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras docker-buildx-plugin docker-model-plugin'
+                $sh_c 'apt-get autoremove -y --purge'
+
+                $sh_c 'rm -f /etc/apt/sources.list.d/docker.list'
+                $sh_c 'rm -f /etc/apt/keyrings/docker.asc'
+
+                echo "Nettoyage des données Docker résiduelles..."
+                $sh_c 'rm -rf /var/lib/docker'
+                $sh_c 'rm -rf /var/lib/containerd'
+            )
+            echo "Docker a été désinstallé avec succès."
+            ;;
+        centos|fedora|rhel)
+            (
+                echo "Désinstallation des packages Docker..."
+                if command_exists dnf; then
+                    $sh_c "dnf remove -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras docker-buildx-plugin docker-model-plugin"
+                    $sh_c "dnf autoremove -y"
+                else
+                    $sh_c "yum remove -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-ce-rootless-extras docker-buildx-plugin docker-model-plugin"
+                    $sh_c "yum autoremove -y"
+                fi
+
+                $sh_c 'rm -f /etc/yum.repos.d/docker-ce.repo'
+
+                echo "Nettoyage des données Docker résiduelles..."
+                $sh_c 'rm -rf /var/lib/docker'
+                $sh_c 'rm -rf /var/lib/containerd'
+            )
+            echo "Docker a été désinstallé avec succès."
+            ;;
+        *)
+            echo
+            echo "ERROR: Distribution '$lsb_dist' non supportée."
+            echo
+            ;;
+    esac
+}
+
+sudo docker stop $(sudo docker ps -aq) || true
+sudo docker rm $(sudo docker ps -aq) || true
+sudo docker volume rm $(sudo docker volume ls -q) || true
+sudo docker network prune -f || true
+sudo docker system prune -a -f || true
+
+sudo k3d cluster delete --all || true
+
+do_uninstall
+
+# kubectl uninstallation
+sudo rm -v $(which kubectl)
+
+# K3d uninstallation
+sudo rm -v $(which k3d)
+
+# argocd uninstallation
+sudo rm -v $(which argocd)
+
+# helm uninstallation
+sudo helm repo remove gitlab || true
+sudo rm -v $(which helm)
