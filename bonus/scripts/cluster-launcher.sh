6d413a81e6caca5bb40e924a5ec8431d317ca019
Noah Alexandre <noah.alxdr+github@gmail.com>
2025-12-15 23:59:34 +0100

    final touch <3

--- bonus/scripts/cluster-launcher.sh
+++ bonus/scripts/cluster-launcher.sh
@@ -4,80 +4,72 @@ set -e
 
 ###
 
-if [ "$(id -u)" -ne 0 ]; then
-	echo "This script must be run as root." >&2
-	exit 1
-fi
-
-echo -e "\e[1;34m* root privileges granted.\e[0m"
-
 if ! command -v docker >/dev/null 2>&1; then
-	echo "Docker is not installed. Please install Docker and try again." >&2
-	exit 1
+  echo "Docker is not installed. Please install Docker and try again." >&2
+  exit 1
 fi
 
 echo -e "\e[1;34m* docker is installed.\e[0m"
 
 if ! command -v k3d >/dev/null 2>&1; then
-	echo "k3d is not installed. Please install k3d and try again." >&2
-	exit 1
+  echo "k3d is not installed. Please install k3d and try again." >&2
+  exit 1
 fi
 
 echo -e "\e[1;34m* k3d is installed.\e[0m"
 
 if ! command -v kubectl >/dev/null 2>&1; then
-	echo "kubectl is not installed. Please install kubectl and try again." >&2
-	exit 1
+  echo "kubectl is not installed. Please install kubectl and try again." >&2
+  exit 1
 fi
 
 echo -e "\e[1;34m* kubectl is installed.\e[0m"
 
 if ! command -v argocd >/dev/null 2>&1; then
-	echo "argocd is not installed. Please install argocd and try again." >&2
-	exit 1
+  echo "argocd is not installed. Please install argocd and try again." >&2
+  exit 1
 fi
 
 echo -e "\e[1;34m* argocd is installed.\e[0m"
 
 if ! command -v helm >/dev/null 2>&1; then
-	echo "helm is not installed. Please install helm and try again." >&2
-	exit 1
+  echo "helm is not installed. Please install helm and try again." >&2
+  exit 1
 fi
 
 echo -e "\e[1;34m* helm is installed.\e[0m"
 
 ###
 
-sudo k3d cluster create --no-lb --agents 0 bonus-cluster
-sudo kubectl create namespace dev
-sudo kubectl create namespace gitlab
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+k3d cluster create --no-lb Inception-of-Things
+kubectl create namespace gitlab
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # ? I - Lancer cette commande pour port forwarding le dashboard Argo CD sur le port 8080 de la machine hôte et y accéder via http://localhost:8080
 # * ```
-# * sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+# * kubectl port-forward svc/argocd-server -n argocd 8080:443
 # * ```
 # ! PORT FORWARDING, BLOQUE LE PROMPT, LANCE LE DASHBOARD ARGO CD SUR LE PORT 8080
 
 # ? II - Récupérer le mot de passe initial pour l'utilisateur admin
 # * ```
-# * sudo argocd admin initial-password -n argocd
+# * argocd admin initial-password -n argocd
 # * ```
 # ? Se connecter au dashboard + CLI avec l'utilisateur admin et le mot de passe récupéré
 
 # ? III - Supprimer le `secret` qui stock le mot de passe initial pour des raisons de sécurité
 # * ```
-# * sudo kubectl delete secret -n argocd argocd-initial-admin-secret
+# * kubectl delete secret -n argocd argocd-initial-admin-secret
 # * ```
 
 # ? IV - Se connecter via Argo CD CLI
 # * ```
-# * sudo argocd login localhost:8080 --username admin --password <password>
+# * argocd login localhost:8080 --username admin --password <password>
 # * ```
 
 # ? V - Changer le mot de passe de l'utilisateur admin
 # * ```
-# * sudo argocd account update-password
+# * argocd account update-password
 # * ```
 
 tput setaf 4
@@ -96,9 +88,9 @@ fi
 rm -fv cosign.pub
 
 echo "Installing GitLab Runners via Helm Chart..."
-sudo helm repo add gitlab https://charts.gitlab.io/ --namespace gitlab
-sudo helm repo update
-sudo helm upgrade --install gitlab gitlab/gitlab \
+helm repo add gitlab https://charts.gitlab.io/ --namespace gitlab
+helm repo update
+helm upgrade --install gitlab gitlab/gitlab \
   --timeout 600s \
   --set certmanager-issuer.email=marwan0620@gmail.com \
   --set global.hosts.domain=bonus.com \
@@ -116,11 +108,12 @@ tput sgr0
 read -n 1 -s -r -p ""
 echo ""
 
-sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode ; echo
+kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath='{.data.password}' | base64 --decode
+echo
 
 # ? VI - Accéder à GitLab via l'URL : http://gitlab.bonus.com
 # * ```
-# * sudo kubectl port-forward -n gitlab svc/gitlab-webservice-default 8081:8181
+# * kubectl port-forward -n gitlab svc/gitlab-webservice-default 8081:8181
 # * ```
 # ! PORT FORWARDING, BLOQUE LE PROMPT, LANCE GITLAB SUR LE PORT 8081
 
--- p3/scripts/cluster-launcher.sh
+++ p3/scripts/cluster-launcher.sh
@@ -4,67 +4,61 @@ set -e
 
 ###
 
-if [ "$(id -u)" -ne 0 ]; then
-	echo "This script must be run as root." >&2
-	exit 1
-fi
-
 if ! command -v docker >/dev/null 2>&1; then
-	echo "Docker is not installed. Please install Docker and try again." >&2
-	exit 1
+  echo "Docker is not installed. Please install Docker and try again." >&2
+  exit 1
 fi
 
 if ! command -v k3d >/dev/null 2>&1; then
-	echo "k3d is not installed. Please install k3d and try again." >&2
-	exit 1
+  echo "k3d is not installed. Please install k3d and try again." >&2
+  exit 1
 fi
 
 if ! command -v kubectl >/dev/null 2>&1; then
-	echo "kubectl is not installed. Please install kubectl and try again." >&2
-	exit 1
+  echo "kubectl is not installed. Please install kubectl and try again." >&2
+  exit 1
 fi
 
 if ! command -v argocd >/dev/null 2>&1; then
-	echo "argocd is not installed. Please install argocd and try again." >&2
-	exit 1
+  echo "argocd is not installed. Please install argocd and try again." >&2
+  exit 1
 fi
 
 ###
 
-sudo k3d cluster create --no-lb --agents 0 p3-cluster
-sudo kubectl create namespace dev
-sudo kubectl create namespace argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+k3d cluster create --no-lb Inception-of-Things
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # ? I - Lancer cette commande pour port forwarding le dashboard Argo CD sur le port 8080 de la machine hôte et y accéder via http://localhost:8080
 # * ```
-# * sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
+# * kubectl port-forward svc/argocd-server -n argocd 8080:443
 # * ```
 # ! PORT FORWARDING, BLOQUE LE PROMPT, LANCE LE DASHBOARD ARGO CD SUR LE PORT 8080
 
 # ? II - Récupérer le mot de passe initial pour l'utilisateur admin
 # * ```
-# * sudo argocd admin initial-password -n argocd
+# * argocd admin initial-password -n argocd
 # * ```
 # ? Se connecter au dashboard + CLI avec l'utilisateur admin et le mot de passe récupéré
 
 # ? III - Supprimer le `secret` qui stock le mot de passe initial pour des raisons de sécurité
 # * ```
-# * sudo kubectl delete secret -n argocd argocd-initial-admin-secret
+# * kubectl delete secret -n argocd argocd-initial-admin-secret
 # * ```
 
 # ? IV - Se connecter via Argo CD CLI
 # * ```
-# * sudo argocd login localhost:8080 --username admin --password <password>
+# * argocd login localhost:8080 --username admin --password <password>
 # * ```
 
 # ? V - Changer le mot de passe de l'utilisateur admin
 # * ```
-# * sudo argocd account update-password
+# * argocd account update-password
 # * ```
 
 # ? VI - Ajouter le cluster k3d au CLI Argo CD
 # * ```
-# * sudo argocd cluster add k3d-p3-cluster
+# * argocd cluster add k3d-p3-cluster
 # * ```
 # ! S'il y a un probleme lors de l'ajout du cluster k3d au CLI Argo CD, consulter : https://github.com/Nimpoo/Inception-of-Things/wiki/V-%E2%80%90-ArgoCD,-CI-CD,-GitOps-Model#warning
 # ! COMMANDE NON NECESSAIRE, VOIR ICI : https://github.com/Nimpoo/Inception-of-Things/wiki/V-%E2%80%90-ArgoCD,-CI-CD,-GitOps-Model#important-edit
